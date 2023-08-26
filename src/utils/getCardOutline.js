6fda281028c2814626f746402110d52cecc7d78b
Malberee <82259650+Malberee@users.noreply.github.com>
2023-08-26 20:19:26 +0300

    Update getCardOutline.js

--- src/utils/getCardOutline.js
+++ src/utils/getCardOutline.js
@@ -1,7 +1,9 @@
 export const getCardOutline = (classes) => {
+	console.log(classes)
 	if (classes.length > 1) {
+		console.log(`${classes[0]}_${classes[1]}`)
 		return `${classes[0]}_${classes[1]}`
 	}
-
+	console.log(classes)
 	return classes[0]
 }
