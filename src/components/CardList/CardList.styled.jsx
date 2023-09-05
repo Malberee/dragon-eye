105d9be0052f1cdc34a47cb33d4eeb355f327c7b
Malberee <82259650+Malberee@users.noreply.github.com>
2023-09-05 22:07:37 +0300

    fixes

--- src/components/CardList/CardList.styled.jsx
+++ src/components/CardList/CardList.styled.jsx
@@ -3,11 +3,14 @@ import styled from 'styled-components'
 export const CardListWrapper = styled.div`
 	height: 100%;
 	width: 100%;
+	padding: 30px 0;
+
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	flex-wrap: wrap;
 	gap: 16px;
+
 	overflow: auto;
 `
 
