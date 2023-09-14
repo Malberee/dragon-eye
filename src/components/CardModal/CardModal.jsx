af7676f6cfc605c5a9b65e4aecf761612d35691b
Malberee <82259650+Malberee@users.noreply.github.com>
2023-09-14 14:27:08 +0300

    🔧 Fix tooltips

--- src/components/CardModal/CardModal.jsx
+++ src/components/CardModal/CardModal.jsx
@@ -16,165 +16,173 @@ import ClassIcon from '../ClassIcon'
 import ClassIconsMiniBack from '../ClassIconsMiniBack/ClassIconsMiniBack'
 
 import {
-	CardOutline,
-	CardWrapper,
-	CardHeader,
-	DragonName,
-	DragonPicture,
-	CardInner,
-	Salvo,
-	SalvoIcon,
-	SalvoType,
-	Abilities,
-	DragonDescription,
-	DragonDescriptionWrapper,
+  CardOutline,
+  CardWrapper,
+  CardHeader,
+  DragonName,
+  DragonPicture,
+  CardInner,
+  Salvo,
+  SalvoIcon,
+  SalvoType,
+  Abilities,
+  DragonDescription,
+  DragonDescriptionWrapper,
 } from '../Card/Card.styled'
 import {
-	CardBackWrapper,
-	CardBackClassIconWrapper,
-	TooltipLink,
+  CardBackWrapper,
+  CardBackClassIconWrapper,
+  TooltipLink,
 } from './CardModal.styled'
 
 const CardModal = (props, ref) => {
-	const dragon = useSelector((state) => state.dragons.selectedDragon)
+  const dragon = useSelector((state) => state.dragons.selectedDragon)
 
-	const [isFlipped, setIsFlipped] = useState(false)
+  const [isFlipped, setIsFlipped] = useState(false)
 
-	useImperativeHandle(ref, () => ({
-		setIsFlipped: (value) => setIsFlipped(value),
-		isFlipped,
-	}))
+  useImperativeHandle(ref, () => ({
+    setIsFlipped: (value) => setIsFlipped(value),
+    isFlipped,
+  }))
 
-	const {
-		name,
-		classes,
-		size,
-		fireType,
-		abilities,
-		picture,
-		description,
-		rarity,
-	} = dragon
+  const {
+    name,
+    classes,
+    size,
+    fireType,
+    abilities,
+    picture,
+    description,
+    rarity,
+  } = dragon
 
-	return (
-		<Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} tiltReverse={true}>
-			<ReactCardFlip
-				isFlipped={isFlipped}
-				flipDirection="horizontal"
-				cardStyles={{
-					front: {
-						transformStyle: 'none',
-					},
-					back: {
-						transformStyle: 'none',
-					},
-				}}
-			>
-				<CardOutline
-					outline={getCardOutline(classes)}
-					onClick={() => setIsFlipped((x) => !x)}
-				>
-					<CardWrapper>
-						<CardHeader>
-							<TooltipLink
-								data-tooltip-id="my-tooltip"
-								data-tooltip-content={`Type: ${size.type}\n\t Size: ${size.number}`}
-							>
-								<SizeIcon type={size.type} number={size.number} />
-							</TooltipLink>
-							<TooltipLink
-								data-tooltip-id="my-tooltip"
-								data-tooltip-content={name}
-							>
-								<DragonName>{name}</DragonName>
-							</TooltipLink>
-							<TooltipLink
-								data-tooltip-id="my-tooltip"
-								data-tooltip-content={
-									classes.length > 1
-										? `Classes: ${classes.join(' and ')}`
-										: `Class: ${classes[0]}`
-								}
-							>
-								<ClassIconsMini classes={classes} />
-							</TooltipLink>
-						</CardHeader>
+  return (
+    <Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} tiltReverse={true}>
+      <ReactCardFlip
+        isFlipped={isFlipped}
+        flipDirection="horizontal"
+        cardStyles={{
+          front: {
+            transformStyle: 'none',
+          },
+          back: {
+            transformStyle: 'none',
+          },
+        }}
+      >
+        <CardOutline
+          outline={getCardOutline(classes)}
+          onClick={() => setIsFlipped((x) => !x)}
+        >
+          <CardWrapper>
+            <CardHeader>
+              <TooltipLink
+                onClick={(e) => e.stopPropagation()}
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={`Type: ${size.type}\n\t Size: ${size.number}`}
+              >
+                <SizeIcon type={size.type} number={size.number} />
+              </TooltipLink>
+              <TooltipLink
+                onClick={(e) => e.stopPropagation()}
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={name}
+              >
+                <DragonName>{name}</DragonName>
+              </TooltipLink>
+              <TooltipLink
+                onClick={(e) => e.stopPropagation()}
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={
+                  classes.length > 1
+                    ? `Classes: ${classes.join(' and ')}`
+                    : `Class: ${classes[0]}`
+                }
+              >
+                <ClassIconsMini classes={classes} />
+              </TooltipLink>
+            </CardHeader>
 
-						<DragonPicture src={getDragonPicture(name)} />
+            <DragonPicture src={getDragonPicture(name)} />
 
-						<CardInner>
-							<Salvo>
-								<SalvoIcon src="./images/icons/salvo.svg" />
-								<TooltipLink
-									data-tooltip-id="my-tooltip"
-									data-tooltip-content={fireType}
-								>
-									<SalvoType>{fireType}</SalvoType>
-								</TooltipLink>
-							</Salvo>
+            <CardInner>
+              <Salvo>
+                <SalvoIcon src="./images/icons/salvo.svg" />
+                <TooltipLink
+                  onClick={(e) => e.stopPropagation()}
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content={fireType}
+                >
+                  <SalvoType>{fireType}</SalvoType>
+                </TooltipLink>
+              </Salvo>
 
-							<Abilities>
-								{abilities.map((ability) => (
-									<TooltipLink
-										key={uid()}
-										data-tooltip-id="my-tooltip"
-										data-tooltip-content={ability}
-									>
-										<AbilityIcon key={uid()} ability={ability} width={18} />
-									</TooltipLink>
-								))}
-							</Abilities>
-						</CardInner>
-						<TooltipLink
-							data-tooltip-id="my-tooltip"
-							data-tooltip-content={description}
-						>
-							<DragonDescriptionWrapper>
-								<DragonDescription>{description}</DragonDescription>
-							</DragonDescriptionWrapper>
-						</TooltipLink>
+              <Abilities>
+                {abilities.map((ability) => (
+                  <TooltipLink
+                    key={uid()}
+                    onClick={(e) => e.stopPropagation()}
+                    data-tooltip-id="my-tooltip"
+                    data-tooltip-content={
+                      ability.charAt(0).toUpperCase() + ability.slice(1)
+                    }
+                  >
+                    <AbilityIcon key={uid()} ability={ability} width={18} />
+                  </TooltipLink>
+                ))}
+              </Abilities>
+            </CardInner>
+            <TooltipLink
+              onClick={(e) => e.stopPropagation()}
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content={description}
+            >
+              <DragonDescriptionWrapper>
+                <DragonDescription>{description}</DragonDescription>
+              </DragonDescriptionWrapper>
+            </TooltipLink>
 
-						<TooltipLink
-							data-tooltip-id="my-tooltip"
-							data-tooltip-content={`Rarity: ${rarity}`}
-						>
-							<RaritySticker rarity={rarity} withLink={true} />
-						</TooltipLink>
-					</CardWrapper>
-				</CardOutline>
+            <TooltipLink
+              onClick={(e) => e.stopPropagation()}
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content={`Rarity: ${rarity}`}
+            >
+              <RaritySticker rarity={rarity} withLink={true} />
+            </TooltipLink>
+          </CardWrapper>
+        </CardOutline>
 
-				<CardOutline
-					outline={getCardOutline(classes)}
-					onClick={() => setIsFlipped((x) => !x)}
-					style={{ transform: 'scaleX(-1)' }}
-				>
-					<CardBackWrapper countClasses={classes.length}>
-						<CardBackClassIconWrapper>
-							{classes.map((dragonClass) => (
-								<ClassIcon
-									key={uid()}
-									dragonClass={dragonClass}
-									countClasses={classes.length}
-								/>
-							))}
-						</CardBackClassIconWrapper>
-						<ClassIconsMiniBack classes={classes} />
-					</CardBackWrapper>
-				</CardOutline>
-			</ReactCardFlip>
-			<Tooltip
-				id="my-tooltip"
-				style={{
-					fontFamily: 'Roboto',
-					textTransform: 'none',
-					height: 'auto',
-					maxWidth: '400px',
-					whiteSpace: 'pre-line',
-					textAlign: 'left',
-				}}
-			/>
-		</Tilt>
-	)
+        <CardOutline
+          outline={getCardOutline(classes)}
+          onClick={() => setIsFlipped((x) => !x)}
+          style={{ transform: 'scaleX(-1)' }}
+        >
+          <CardBackWrapper countClasses={classes.length}>
+            <CardBackClassIconWrapper>
+              {classes.map((dragonClass) => (
+                <ClassIcon
+                  key={uid()}
+                  dragonClass={dragonClass}
+                  countClasses={classes.length}
+                />
+              ))}
+            </CardBackClassIconWrapper>
+            <ClassIconsMiniBack classes={classes} />
+          </CardBackWrapper>
+        </CardOutline>
+      </ReactCardFlip>
+      <Tooltip
+        id="my-tooltip"
+        style={{
+          fontFamily: 'Roboto',
+          height: 'auto',
+          maxWidth: '350px',
+          whiteSpace: 'pre-line',
+          textAlign: 'center',
+        }}
+      />
+    </Tilt>
+  )
 }
 
 export default forwardRef(CardModal)
